f1e4a520b12bc070e8aa8e8b4c1574c8fd2c3a9d
niemiao <2271131064@qq.com>
2020-12-28 18:44:33 +0800

    update

---
+++ get_data.py
@@ -0,0 +1,3 @@
+#只需要传入test_case对应的function name，即可获取request data
+import json
+def get_data(func):
---
+++ test_login.py
@@ -0,0 +1,21 @@
+import unittest
+import requests
+import json
+
+class TestLogin(unittest.TestCase):
+    def setUP(self):
+        print("······测试开始······")
+
+    def test_login_0(self):
+        #正确的账号密码
+        url = ""
+        method = ""
+        data =
+        request
+        assert
+
+    def tearDown(self):
+        print("······测试结束·····")
+
+if __name__ == '__main__':
+    unittest.main()
